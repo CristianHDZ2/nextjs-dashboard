37fe1724ac5c304a60d1889ff84e34a29f33b596
CristianHDZ2 <Cristianhernandez.csv@gmail.com>
2025-04-07 10:40:44 -0600

    1

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -16,8 +16,7 @@ const iconMap = {
 export default async function CardWrapper() {
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
+      {/* NOTE: Uncomment this code when you need it */}
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
